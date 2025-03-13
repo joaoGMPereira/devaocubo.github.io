0b9ea0001da5ddb7c4b5c9e4d90d8e7d1f087bc3
Joao Gabriel de Medeiros Pereira <gah.mp1@gmail.com>
2025-03-13 11:14:40 -0300

    ajuste de nomes

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "IgniteStarter",
+    name: "DevAoCubo",
     platforms: [.macOS(.v13)],
     dependencies: [
         .package(url: "https://github.com/twostraws/Ignite.git", branch: "main")
     ],
     targets: [
         .executableTarget(
-            name: "IgniteStarter",
+            name: "DevAoCubo",
             dependencies: ["Ignite"]),
     ]
 )
